60aba17187dd8f26f6ed19af551514e0aa93207c
Emmanuel Adebayo <emmanueltolu.adebayo@gmail.com>
2023-04-28 15:59:41 +0100

    fix: resolved order

--- src/usecases/events/get-event-orders.use-case.ts
+++ src/usecases/events/get-event-orders.use-case.ts
@@ -18,6 +18,10 @@ export class GetEventOrdersUseCase {
     // Format the orders for each item and compute the floor price for each associated collection
     const formattedOrders = await Promise.all(
       items.items.map(async (order) => {
+        // Check if order.collection.order is null or undefined
+        if (!order.collection.order) {
+          return null;
+        }
         // Extract the relevant fields from the order and its associated collection and token
         const {
           id,
